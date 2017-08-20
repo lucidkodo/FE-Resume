971b4d8729f8ee9c3138a029fc3917b514d2d3b5
lucidkodo <taoffke@gmail.com>
2017-08-21 03:05:18 +0800

    Fine tuning + Query setup
    25% thru queries setup.

---
+++ resources/js/queries.js
@@ -0,0 +1,60 @@
+/* 1201px ~ 1400px */
+@media only screen and (max-width: 1400px) {
+	.connect-with-me { margin-left: 33%; }
+}
+
+/* 1061px ~ 1200px */
+@media only screen and (max-width: 1200px) {
+	.connect-with-me { margin-left: 30.5%; }
+}
+
+/* 1024 ~ 1060 */
+@media only screen and (max-width: 1060px) {
+	.left-panel {
+		height: 73px;
+		width: 100%;
+		background-color: #001b30;
+		position: fixed;
+		z-index: 999;
+	}
+
+	.site-content {
+		width: 100%;
+		margin: 0;
+		padding: 0;
+	}
+
+	.my-photo {
+		display: none;
+	}
+
+	nav {
+		padding: 25px;
+		float: right;
+	}
+
+	nav li {
+		list-style: none;
+		line-height: 100%;
+		display: inline;
+		padding: 0 20px;
+	}
+
+	.connect-with-me { margin-left: 32%; }
+}
+
+/* 768px ~ 1023px */
+@media only screen and (max-width: 1023px) {
+	
+} 
+
+
+/* 481px ~ 767px */
+@media only screen and (max-width: 767px) {
+	
+}
+
+/* 0 ~ 480px */
+@media only screen and (max-width: 480px) {
+	
+}
